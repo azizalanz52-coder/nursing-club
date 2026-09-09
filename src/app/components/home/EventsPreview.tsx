b45ca7622d3f89d7c03e52d3f736639698dc9759
Abdulaziz Alanazi <azizalanz52@gmail.com>
2026-09-09 13:08:44 +0300

    fix events page typescript interface and build error

--- src/app/components/home/EventsPreview.tsx
+++ src/app/components/home/EventsPreview.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { db } from '../lib/firebase';
+import { db } from '../../lib/firebase';
 import { collection, getDocs } from 'firebase/firestore';
 
 const DEFAULT_EVENTS = [
