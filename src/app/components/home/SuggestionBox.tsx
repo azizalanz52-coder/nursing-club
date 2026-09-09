a33b3c419b070e02f06ad2ed1782f1157d7cb06c
Abdulaziz Alanazi <azizalanz52@gmail.com>
2026-09-09 22:40:41 +0300

    comprehensive update across all project pages and features

--- src/app/components/home/SuggestionBox.tsx
+++ src/app/components/home/SuggestionBox.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { db } from '../lib/firebase';
+import { db } from '../../lib/firebase';
 import { doc, setDoc } from 'firebase/firestore';
 
 export default function SuggestionBox() {
