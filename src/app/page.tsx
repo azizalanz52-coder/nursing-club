287e8d62ab61107a5132956417b6d3d089c2abb2
Abdulaziz Alanazi <azizalanz52@gmail.com>
2026-09-21 21:30:05 +0300

    comprehensive update across all project pages and features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
 import Hero from "./components/Hero";
 import About from "./components/home/About";
+import CaseStudyCard from "./components/home/CaseStudyCard";
 import LeadershipPreview from "./components/home/LeadershipPreview";
 import CommitteesPreview from "./components/home/CommitteesPreview";
 import EventsPreview from "./components/home/EventsPreview";
 import WhatsAppCommunity from "./components/home/WhatsAppCommunity";
-import SuggestionBox from "./components/home/SuggestionBox";
 import Footer from "./components/Footer";
 
-export default function Home() {
+export default function TeamPage() {
   return (
-    <main>
+    <main className="min-h-screen bg-slate-50 text-slate-800">
       <Hero />
       <About />
+      <CaseStudyCard />
       <LeadershipPreview />
       <CommitteesPreview />
       <EventsPreview />
       <WhatsAppCommunity />
-      <SuggestionBox />
       <Footer />
     </main>
   );
